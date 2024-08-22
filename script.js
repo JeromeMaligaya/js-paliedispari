2d33cc65acdef164f1db091204ad5de2b9d1ecf2
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-08-22 18:12:56 +0200

    completed evento form e generazione output

--- script.js
+++ script.js
@@ -21,6 +21,7 @@ console.log(reverseText(''))
 // 1.recupero l'elemento dal Dom
 const formPali = document.querySelector('form');
 const wordField = document.getElementById('wordPali');
+const targertPali = document.querySelector('.targetPali');
 
 //2.raccolta dati
   
@@ -32,16 +33,26 @@ formPali.addEventListener('submit', function(e){
     e.preventDefault();
 
     // recupero la parola scritta dall'utente
+    const userWord = wordField.value.trim();  
+    console.log('userWord', userWord);
 
     // attivo la funz reverseText con la parola raccolta
+    reverseText(userWord);
+    console.log('reverseWord',reverseText(userWord));
 
     // recupero e riaassegno l'output generato dalla funz
+    reverseWord = reverseText(userWord);
 
     // comparo l'output della funz con la parola dell'utente e verifico se è palidroma
+    let messagePali;
+    
+    let isPalidroma = (userWord === reverseWord) ? messagePali = `La tua parola: <strong>${userWord}</strong>, <span class="text-success">è palidroma</span>.` : messagePali = `La tua parola: <strong>${userWord}</strong>, <span class="text-danger">non è palidroma</span>.`;
+    console.log(messagePali);
 
-    // stampo messagePali
+    // 4.generazione output
+    // stampo message
+    targertPali.innerHTML = messagePali;
 
 })
 
 
-// 4.generazione output
