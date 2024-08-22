fd56da367f8c6a64dffb1afa36f07d79136e1ab7
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-08-22 17:11:10 +0200

    added function reverseText

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 //Check link
 console.log('JS OK')
 
-// 1.recupero l'elemento dal Dom
+// funzione per invertire l'ordine delle lettere di un text/word
 
+function reverseText (text){
+    //creo una stringa da riempire
+    let reverseText = ''; 
+    
+    //creo un ciclo con un contatore che mi parte dalla ultima lettere ed inizia il decremento
+    for (let i = text.length - 1; i >= 0; i--){
+        reverseText += text[i];
+    }
+
+    //dalla funzione mi uscirà la stringa completata
+    return reverseText;
+}
+
+console.log(reverseText(''))
+
+// 1.recupero l'elemento dal Dom
 
 //2.raccolta dati
     
